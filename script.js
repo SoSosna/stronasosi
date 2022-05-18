5a435ca747680a33ea5933bb51902565d5851f95
SoSosna <black.so.power@gmail.com>
2022-05-18 14:48:49 +0200

    hamburger menu v1

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("menu");
+const btn = document.getElementById("MenuBtn");
+const icon = document.getElementById("Icon");
+
+const showMenu = () => {
+
+    menu.classList.toggle("show");
+    icon.classList.toggle("bi-list");
+    icon.classList.toggle("bi-x-circle");
+
+}
+btn.addEventListener("click", showMenu);
